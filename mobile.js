a489673b8a10aba4a266ee5dea1d8db37c2d010b
james scott <j-scott@github.com>
2020-05-31 16:55:28 +0100

    mobile' git push

--- mobile.js
+++ mobile.js
@@ -1,6 +1,6 @@
 // detects if site is being viewed on mobile
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-	$("#content").css('display', 'none')
+	$("#container").css('display', 'none')
 }
 else {
 	$("#mobile-viewers").css('display', 'none')
